cff2d167d72ff1aa1bda9a200122003f4e0c8987
Steven Almeroth <steven.almeroth@gmail.com>
2025-04-18 13:02:09 -0400

    post

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
-from fasthtml.common import fast_app, serve
+from starlette.requests import Request
+from fasthtml.common import *
 
-app, rt = fast_app()
+dev_config = {
+    "live": True,
+    "debug": True,
+}
+
+app, rt = fast_app(**dev_config)
 serve()
 
 
 @rt
 def index():
-    return "Hello, World!"
+    return Titled("SnapGrid", 
+        Div(
+            Form(
+                Input(type="text", name="url", required=True, placeholder="Enter URL", style="padding: 8px"),
+                Button("Snap", type="submit"),
+                Div(id="result", style="margin-top: 20px;"),
+                hx_post="/fetch-url",
+                hx_target="#result",
+            )
+        )
+    )
+
+@rt("/fetch-url")
+async def _(request: Request):
+    form = await request.form()
+    url = form.get('url')
+    if not url:
+        return "Please enter a URL"
+    return f"Fetching URL: {url}"
