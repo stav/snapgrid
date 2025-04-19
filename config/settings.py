cdac9223261ba58b1e8808b453770af7a23a2aa9
Steven Almeroth <steven.almeroth@gmail.com>
2025-04-19 11:46:58 -0400

    normal style

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,6 @@
-import os
 import configparser
 from pathlib import Path
+from fasthtml.common import Link, StyleX
 
 # Base directory
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -10,8 +10,12 @@ base_config = {
     "host": "0.0.0.0",
     "port": 8001,
     "debug": True,
-    "static_dir": str(BASE_DIR / "app" / "static"),
-    "template_dir": str(BASE_DIR / "app" / "templates"),
+    "static_path": str(BASE_DIR / "app"),
+    "template_path": str(BASE_DIR / "app"),
+    "hdrs": [
+        Link(rel="icon", href="/static/favicon.ico"),
+        StyleX("app/styles.css"),
+    ],
 }
 
 dev_config = {
